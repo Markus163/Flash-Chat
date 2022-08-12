259d8f5da0b10f1cc7c795d94b107f2419e88f2e
Markus163 <pprimak@mail.ru>
2022-08-12 19:06:27 +0400

    Final variant

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Марк Михайлов on 09.08.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    
+    let sender: String
+    let body: String
+}
